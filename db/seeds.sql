85e51da9ad706b76210f18cead8a51c8bff62084
Jon Bodnar <jon.bodnar@gmail.com>
2019-11-23 12:35:46 -0500

    Add db schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,128 @@
+USE burgers_db;
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('Gambel''s quail', 1);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('Timber Wolf', 1);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('Laughing Kookaburra', 1);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('Pacific Gull', 0);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('Common Wombat', 1);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('Chuckwalla', 0);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('Arctic lemming', 1);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('White-throated Monitor', 0);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('California Sea Lion', 0);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('Monitor Legaan', 0);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('Crested Bunting', 0);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('Golden Brush-tailed Possum', 0);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('Thirteen-lined Squirrel', 0);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('Desert Spiny Lizard', 0);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('Steenbok', 0);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('Peregrine Falcon', 0);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('Currasow', 1);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('Mocking Cliffchat', 0);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('Savannah Deer', 1);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('Asian Water Buffalo', 0);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('Downy Woodpecker', 0);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('Red-necked Wallaby', 0);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('Tiger', 1);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('Brown pelican', 0);
+
+insert into
+    burgers (burger_name, devoured)
+values
+    ('Purple Grenadier', 0);
+
+SELECT * FROM burgers;
