72b45440f28d7ef9fed50b7a85023ae070e89e46
Jon Bodnar <jon.bodnar@gmail.com>
2019-11-25 12:09:16 -0500

    Update db connection for heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 const mysql = require("mysql2");
+let burgersDB;
 
-const burgersDB = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "docker",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  burgersDB = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  burgersDB = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "docker",
+    database: "burgers_db"
+  });
+}
 
 burgersDB.connect(error => {
   if (error) console.error(err.stack);
